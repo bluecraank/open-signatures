353f320df6215a5dee60da300b077a3b1f9f5b84
Nils Fischer <bluecraank@outlook.de>
2024-12-05 15:03:55 +0000

    Remove open-signage things

--- resources/views/livewire/poll-presentation-process.blade.php
+++
@@ -1,9 +0,0 @@
-<div wire:poll.10s>
-    @foreach ($currentPresentations as $presentation)
-        <span class="processing_info d-flex justify-content-center align-items-center">
-            <span class="loader"></span>
-            <span class="mx-1">{{ __('Processing') }} {{ $presentation->name }}
-                ({{ $presentation->slides()->count() }}/{{ $presentation->total_slides }})</span>
-        </span>
-    @endforeach
-</div>
--- resources/views/unauthorized.blade.php
+++
@@ -1,2 +0,0 @@
-<p class="text-center is-size-4">{{ __('You dont have permission to view data') }}</p>
-<p class="text-center is-size-6">{{ __('If you think this is a mistake, please contact your administrator') }}</p>

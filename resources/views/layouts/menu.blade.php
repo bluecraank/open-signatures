cc043b10d61a789bd66f35f6b6b0fa36bd7acfb0
Nils Fischer <bluecraank@outlook.de>
2025-02-07 11:38:29 +0000

    Remove open-signage related content

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-secondary-subtle mb-5 p-3">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">open-signage</a>
+        <a class="navbar-brand" href="/">open-signatures</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
